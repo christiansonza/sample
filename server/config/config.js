0e3e8286bc2a767230f460f39ddfaab2301ac8dd
christiansonza <103972546+christiansonza@users.noreply.github.com>
2025-10-02 10:54:42 +0800

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // Atlas URI from .env
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
