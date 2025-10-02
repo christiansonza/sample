6d12c91a66b32ce121b24094504f8836c9618e10
christiansonza <103972546+christiansonza@users.noreply.github.com>
2025-10-02 10:22:43 +0800

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // no options needed
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
